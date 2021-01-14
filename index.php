682bc60564a16de46229b2f1b7b29cfa5a6c07c5
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-01-14 04:21:53 +0300

    Home page navigation fixed

--- index.php
+++ index.php
@@ -7,27 +7,31 @@
 </head>
 <body>
 <header>
-
     <div class="navbar">
         <a class="current" href="index.php">Главная</a>
         <a href="list_of_vehicles.php">Модели автомобилей</a>
         <div class="dropdown">
-            <button class="dropbtn">Автомобили в наличии
-                <!--<i class="fa fa-caret-down"></i>-->
-            </button>
+            <button class="dropbtn">Автомобили в наличии</button>
+            <div class="dropdown-content">
+                <a href="for_sale_cars.php?country=RUSSIA">СССР/Россия</a>
+                <a href="for_sale_cars.php?country=UK">Великобритания</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">Обслуживание</button>
+            <div class="dropdown-content">
+                <a href="for_sale_cars.php?country=RUSSIA">Обслуживаемые автомобили</a>
+                <a href="for_sale_cars.php?country=UK">Великобритания</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">Запасные части и расходники</button>
             <div class="dropdown-content">
                 <a href="for_sale_cars.php?country=RUSSIA">СССР/Россия</a>
                 <a href="for_sale_cars.php?country=UK">Великобритания</a>
             </div>
         </div>
     </div>
-
-    <form action="list_of_vehicles.php" method="get" id="nav-list"  novalidate>
-        <p><input type="submit" value="Модели автомобилей"></p>
-    </form>
-    <form action="for_sale_cars.php" method="get" id="nav-sale"  novalidate>
-        <p><input type="submit" value="Автомобили в наличии"></p>
-    </form>
 </header>
 <main>
     <h1>Информационная система салона ретро-автомобилей</h1>
