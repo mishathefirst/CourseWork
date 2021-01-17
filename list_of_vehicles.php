115fe77e9e34cd4309b4c44823ed00c26ba770a1
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-01-17 15:50:37 +0300

    Filter added to list_of_vehicles

--- list_of_vehicles.php
+++ list_of_vehicles.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles/nav-style.css">
+    <link rel="stylesheet" href="styles/filter-style.css">
     <title>Модели ретро-автомобилей</title>
 </head>
 <body>
@@ -38,41 +39,43 @@
     </div>
 </header>
 <main>
-    <form action="list_of_vehicles.php" method="get" id="form"  onsubmit="check(); return true;" novalidate>
+    <br>
+    <div id="filter" class="filter">
+        <div class="filter-content">
+    <h1>Фильтр характеристик:</h1>
+    <form name="input" action="list_of_vehicles.php" method="get">
+        <label for="country_of_production">Страна производства:</label>
+        <select name="country" id="country_of_production">
+            <optgroup label="Countries">
+                <option value="" name="thing">Все</option>
+                <option value="RUSSIA" name="thing">СССР/Россия</option>
+                <option value="UK" name="thing">Великобритания</option>
+                <option value="USA" name="thing">США</option>
+            </optgroup>
+        </select>
+        <br>
+        <label for="min_power">Минимальная мощность двигателя:</label>
+            <input type="range" name="min_power" id="min_power" min="80" max="250" step="10" value="<?php $power_value = (empty($_GET['min_power']))? 80 : $_GET['min_power']; echo $power_value; ?>">
+        <br>
+        <input type="submit" class="submit-button" value="Применить">
+        <br>
+        <h2>     </h2>
 
-        <label class="instructions" for="hidden_x"> Insert X: </label>
-        <button type="button" class="button" name="button_x" value="-4" id="x_-4" onclick="document.getElementById('hidden_x').value = -4"> -4 </button>
-        <button type="button" class="button" name="button_x" value="-3" id="x_-3" onclick="document.getElementById('hidden_x').value = -3"> -3 </button>
-        <button type="button" class="button" name="button_x" value="-2" id="x_-2" onclick="document.getElementById('hidden_x').value = -2"> -2 </button>
-        <button type="button" class="button" name="button_x" value="-1" id="x_-1" onclick="document.getElementById('hidden_x').value = -1"> -1 </button>
-        <button type="button" class="button" name="button_x" value="0" id="x_0" onclick="document.getElementById('hidden_x').value = 0"> 0 </button>
-        <button type="button" class="button" name="button_x" value="1" id="x_1" onclick="document.getElementById('hidden_x').value = 1"> 1 </button>
-        <button type="button" class="button" name="button_x" value="2" id="x_2" onclick="document.getElementById('hidden_x').value = 2"> 2 </button>
-        <button type="button" class="button" name="button_x" value="3" id="x_3" onclick="document.getElementById('hidden_x').value = 3"> 3 </button>
-        <button type="button" class="button" name="button_x" value="4" id="x_4" onclick="document.getElementById('hidden_x').value = 4"> 4 </button>
-        <input type="hidden" name="hidden_x" id="hidden_x" value=""> <br>
-        <p id="warningX" class="toWarnPoints"></p><br>
-
-
-        <label for="yCoordinate" class="instructions"> Insert Y: </label>
-        <input type="text" id="yCoordinate" name="yCoordinate" autocomplete="off">
-        <p id="warning" class="toWarnPoints"></p><br>
-        <p id="warningY" class="toWarnPoints"></p><br>
-
-        <label class="instructions" for="hidden_r"> Insert R: </label>
-        <button type="button" class="button" name="button_r" value="1" id="r_1" onclick="document.getElementById('hidden_r').value = 1"> 1 </button>
-        <button type="button" class="button" name="button_r" value="2" id="r_2" onclick="document.getElementById('hidden_r').value = 2"> 2 </button>
-        <button type="button" class="button" name="button_r" value="3" id="r_3" onclick="document.getElementById('hidden_r').value = 3"> 3 </button>
-        <button type="button" class="button" name="button_r" value="4" id="r_4" onclick="document.getElementById('hidden_r').value = 4"> 4 </button>
-        <button type="button" class="button" name="button_r" value="5" id="r_5" onclick="document.getElementById('hidden_r').value = 5"> 5 </button>
-        <input type="hidden" name="hidden_r" id="hidden_r" value=""> <br>
-        <p id="warningR" class="toWarnPoints"></p><br>
-
-        <p id="button" class="input_button">
-            <input type="submit" value="Check the result.">
-        </p>
     </form>
-    </main>
+        </div>
+    </div>
+</main>
+
+
+
+<div id="table">
+    <h1>Модели:</h1>
+
+
+
+
+
+
 <?php
 $dbconn = pg_connect("host=localhost dbname=postgres user=postgres password=12032001")
 or die('Не удалось соединиться: ' . pg_last_error());
@@ -121,6 +124,6 @@ pg_close($dbconn);
 $previous_query = 0;
 ?>
 
-
+</div>
 </body>
 </html>
